f2fb79b8366deea090be46ccc920212761f997b8
Alexandru-Andrei Cirjaliu-Davidescu <A.A.Cirjaliu-Davidescu@student.tudelft.nl>
2026-04-04 06:26:48 +0800

    Increased partition count

--- create_topics_large.sh
+++ create_topics_large.sh
@@ -10,11 +10,11 @@ topics=(
 )
 
 for topic in "${topics[@]}"; do
-  echo "Creating Kafka topic: $topic (12 partitions)"
+  echo "Creating Kafka topic: $topic (15 partitions)"
   kafka-topics --create \
     --if-not-exists  \
     --topic "$topic" \
     --bootstrap-server kafka:9092 \
-    --partitions 12 \
+    --partitions 15 \
     --replication-factor 1
 done
