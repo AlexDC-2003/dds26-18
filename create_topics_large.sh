97a195bc72eece6d3d161450604ab35c688af2ba
Alexandru-Andrei Cirjaliu-Davidescu <A.A.Cirjaliu-Davidescu@student.tudelft.nl>
2026-04-04 06:06:28 +0800

    Large clusters

---
+++ create_topics_large.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+topics=(
+  "order"
+  "stock.commands"
+  "stock.replies"
+  "payment.commands"
+  "payment.replies"
+  "order.events"
+  "orchestrator.commands"
+  "orchestrator.replies"
+)
+
+for topic in "${topics[@]}"; do
+  echo "Creating Kafka topic: $topic (12 partitions)"
+  kafka-topics --create \
+    --if-not-exists  \
+    --topic "$topic" \
+    --bootstrap-server kafka:9092 \
+    --partitions 12 \
+    --replication-factor 1
+done
